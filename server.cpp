90aef90fc7a0505b556b50ed7385d82855e3255c
Hallo Weeks <96823158+halloweeks@users.noreply.github.com>
2024-02-29 16:31:34 +0530

    Add files via upload

--- server.cpp
+++ server.cpp
@@ -1,60 +1,45 @@
-#include <iostream> // input output
-#include <string> // strlen string
+#include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <pthread.h> // multi threading
+#include <pthread.h>
 #include <errno.h>
 #include <signal.h>
-
-// Included for count execution time
 #include <time.h>
-#include <iomanip>
+#include <stdlib.h>
 
-// server address
 #define ADDRESS "0.0.0.0"
+#define PORT 8080
+#define CONCURRENT_CONNECTION 200
+#define QUEUE_CONNECTION 100
+#define BUFFER_SIZE 65356
+#define THREAD_STACK_SIZE 524288
 
-// port number
-#define PORT 8888
+pthread_mutex_t connection_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-// maximum concurrent connections
-#define CONCURRENT_CONNECTION 10
-
-// maximum connection requests queued
-#define QUEUE_CONNECTION 20
-
-// buffer size 1KB
-#define BUFFER_SIZE 1024
-
-// Thread stack size 64KB
-#define THREAD_STACK_SIZE 65536
-
-// current connections
 int connection = 0;
 
-// connection handler function
 void *connection_handler(void*);
 
 int main(int argc, char *argv[]) {
-	// thread identifier
 	pthread_t thread_id;
-	// thread attribute
+	
 	pthread_attr_t attr;
-        
+	
 	if (pthread_attr_init(&attr) != 0) {
-		std::cout << "[ERROR][THREAD][INIT] " << strerror(errno) << "\n";
-		return -1;
+		printf("[ERROR][THREAD][INIT] %s\n", strerror(errno));
+		return 1;
 	}
-        
-	// stack size 1MB
+	
 	if (pthread_attr_setstacksize(&attr, THREAD_STACK_SIZE) != 0) {
-		std::cout << "[ERROR][THREAD][STACK] " << strerror(errno) << "\n";
-		return -1;
+		printf("[ERROR][THREAD][STACK] %s\n", strerror(errno));
+		return 1;
 	}
 	
 	if (pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED) != 0) {
-		std::cout << "[ERROR][THREAD][DETACH] " << strerror(errno) << "\n";
-		return -1;
+		printf("[ERROR][THREAD][DETACH] %s\n", strerror(errno));
+		return 1;
 	}
 	
 	int master_socket, conn_id;
@@ -63,135 +48,108 @@ int main(int argc, char *argv[]) {
 	memset(&server, 0, sizeof(server));
 	memset(&client, 0, sizeof(client));
 	
-        signal(SIGPIPE, SIG_IGN);
-        
-	// creating master socket
+	signal(SIGPIPE, SIG_IGN);
+	
 	if ((master_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		std::cout << "[ERROR] CAN'T CREATE TO SOCKET\n";
-	    return -1;
+		printf("[ERROR] CAN'T CREATE TO SOCKET\n");
+		return 1;
 	} else {
-		std::cout << "[NOTE] SOCKET CREATED DONE\n";
+		printf("[NOTE] SOCKET CREATED DONE\n");
 	}
 	
-	
-     // Prepare the sockaddr_in structure
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = inet_addr(ADDRESS);
 	server.sin_port = htons(PORT);
-    
+	
 	socklen_t addrlen = sizeof(struct sockaddr_in);
-    
-	// binding address and port
-	if (bind(master_socket, (struct sockaddr *)&server , sizeof(server)) == -1) {
-		std::cout << "[ERROR][BIND] " << strerror(errno) << "\n";
-	    return -1;
+	
+	if (bind(master_socket, (struct sockaddr *)&server, sizeof(server)) == -1) {
+		printf("[ERROR][BIND] %s\n", strerror(errno));
+		return 1;
 	} else {
-		std::cout << "[NOTE] BIND " << ADDRESS << ":" << PORT << "\n";
+		printf("[NOTE] BIND %s:%d\n", ADDRESS, PORT);
 	}
 	
-	// Listen on the socket, with 20 max connection requests queued
 	if (listen(master_socket, QUEUE_CONNECTION) == -1) {
-		std::cout << "[ERROR][LISTEN] " << strerror(errno) << "\n";
-		return -1;
+		printf("[ERROR][LISTEN] %s\n", strerror(errno));
+		return 1;
 	} else {
-		std::cout << "[INFO] WAITING FOR INCOMING CONNECTIONS\n";
+		printf("[INFO] WAITING FOR INCOMING CONNECTIONS\n");
 	}
-    
-	// infinity loop
-	while (true) {
-		// accept new connections
-		conn_id = accept(master_socket, (struct sockaddr*)&client, (socklen_t*)&addrlen);
+	
+	while (1) {
+		conn_id = accept(master_socket, (struct sockaddr *)&client, (socklen_t *)&addrlen);
 		
-        // if connection acception failed
-        if (conn_id == -1) {
-        	std::cout << "[WARNING] CAN'T ACCEPT NEW CONNECTION\n";
-        } else {
-        	 // if connection limit reached
-        	 if (connection >= CONCURRENT_CONNECTION) {
-        	     std::cout << "[WARNING] CONNECTION LIMITE REACHED\n";
-                     // send(conn_id, "server is busy. please try again later.", 39, 0);
-                     close(conn_id); // close connection
-        	 } else {
-        	     std::cout << "[INFO] NEW CONNECTION ACCEPTED FROM " << inet_ntoa(client.sin_addr) << ":" << ntohs(client.sin_port) << "\n";
-                     // create new thread for new connection
-                     if (pthread_create(&thread_id, &attr, connection_handler, new int(conn_id)) == -1) {
-                         // std::cout << "[WARNING][THREAD] " << strerror(errno) << "\n";
-                	     std::cout << "[WARNING] CAN'T CREATE NEW THREAD\n";
-                         close(conn_id);
-                     } else {
-                     	std::cout << "[INFO] NEW THREAD CREATED\n";
-                         connection++; // increase connection count
-                     }
-                 }
-             }
+		if (conn_id == -1) {
+			printf("[WARNING] CAN'T ACCEPT NEW CONNECTION\n");
+		} else {
+			if (connection >= CONCURRENT_CONNECTION) {
+				printf("[WARNING] CONNECTION LIMIT REACHED\n");
+				close(conn_id);
+			} else {
+				int *conn_id_heap = (int*)malloc(sizeof(int));
+				
+				if (conn_id_heap == NULL) {
+					perror("[ERROR] Memory allocation failed");
+					close(conn_id);
+					continue;
+				}
+				
+				*conn_id_heap = conn_id;
+				
+				printf("[INFO] NEW CONNECTION ACCEPTED FROM %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+				
+                if (pthread_create(&thread_id, &attr, connection_handler, (void*)conn_id_heap) == -1) {
+                    printf("[WARNING] CAN'T CREATE NEW THREAD\n");
+                    close(conn_id);
+                    free(conn_id_heap);
+                } else {
+                    printf("[INFO] NEW THREAD CREATED\n");
+                    connection++;
+                }
+            }
         }
+    }
+    
     return 0;
 }
 
-// This will handle connection for each client
 void *connection_handler(void *sock_fd) {
-	/* clock_t clock(void) returns the number of clock ticks 
-	   elapsed since the program was launched.To get the number  
-           of seconds used by the CPU, you will need to divide by  
-           CLOCKS_PER_SEC.where CLOCKS_PER_SEC is 1000000 on typical 
-           32 bit system.  */
-	clock_t start, end; 
+	int conn_id = *((int *)sock_fd);
+	free(sock_fd);
 	
-	// Recording the starting clock tick.
-	start = clock(); 
+	clock_t start, end;
+	start = clock();
 	
-	// byte size
-	int read_byte = 0;
-	
-	// Get the socket descriptor
-	int conn_id = *(int*)sock_fd;
-	
-	// request data
-	char buffer[BUFFER_SIZE] = {0};
-	
-	// response data
-	char response[] = "Hello";
-	
-	// read response continue
-	while ((read_byte = recv(conn_id, buffer, BUFFER_SIZE, 0)) > 0) {
-		std::cout << "[RECEIVED] " << buffer << "\n";
-		// clear buffer data
-		memset(buffer, 0, BUFFER_SIZE);
-		
-		// send response
-		if (send(conn_id, response, strlen(response), 0) > 0) {
-			std::cout << "[SEND] " << response << "\n";
-		} else {
-			std::cout << "[WARNING][SEND] " << strerror(errno) << "\n";
-		}
-		
-	}
-	
-	// terminate connection
-	close(conn_id);
-	std::cout << "[INFO] CONNECTION CLOSED\n";
-	
-	// decrease connection counts
-	connection--;
-	
-	// thread automatically terminate after exit connection handler
-	std::cout << "[INFO] THREAD TERMINATED" << std::endl;
-	
-	delete (int*)sock_fd;
-	
-	// Recording the end clock tick. 
-	end = clock();
+    char buffer[BUFFER_SIZE] = {0};
     
-	// Calculating total time taken by the program. 
-	double time_taken = double(end - start) / double(CLOCKS_PER_SEC); 
+    char response[1024] = "user=hello&pass=1234\n";
+
+    while (recv(conn_id, buffer, BUFFER_SIZE, 0) > 0) {
+    	printf("[RECEIVED] %s\n", buffer);
+        memset(buffer, 0, BUFFER_SIZE);
+
+        if (send(conn_id, response, strlen(response), 0) > 0) {
+            printf("[SEND] %s\n", response);
+        } else {
+            printf("[WARNING][SEND] %s\n", strerror(errno));
+        }
+    }
+
+    close(conn_id);
     
-	// print process time
-	std::cout << "[TIME] PROCESS COMPLETE IN " << std::fixed << time_taken << std::setprecision(5); 
-	std::cout << " SEC\n";
-	
-	// print line
-	std::cout << "------------------------\n";
-	
-	// exiting
-	pthread_exit(NULL);
-} 
+    printf("[INFO] CONNECTION CLOSED\n");
+    
+    pthread_mutex_lock(&connection_mutex);
+    connection--;
+    pthread_mutex_unlock(&connection_mutex);
+   
+    printf("[INFO] THREAD TERMINATED\n");
+    
+    end = clock();
+    double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("[TIME] PROCESS COMPLETE IN %.5f SEC\n", time_taken);
+    printf("------------------------\n");
+    
+    pthread_exit(NULL);
+}
